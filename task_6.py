1a6bdf94d79ce2f3faca349ed1386c5000c3afdb
Aalekseika <aleksieienkonatalia@gmail.com>
2016-11-21 00:14:14 +0200

    fixed division by zero bug

--- task_6.py
+++ task_6.py
@@ -1,8 +1,11 @@
-print('=======================')
+print('************************')
 print('Task #6')
-print('=======================')
+print('************************')
 
 a=10
 b=2
-c=a/b
-print("Result a/b (a=%d, b=%d) is equal %.2f" % (a,b,c))
+if b!=0:
+    c=a/b
+    print("Result a/b (a=%d, b=%d) is equal %.2f" % (a,b,c))
+else:
+    print("Error:Division by zero!")
