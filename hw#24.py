1c9019e0df42fa9d75a1fa71cc99b7f57bbe8d5e
Aalekseika <aleksieienkonatalia@gmail.com>
2016-12-04 22:14:52 +0200

    Commit#24

---
+++ hw#24.py
@@ -0,0 +1,13 @@
+def even_odd_difference(lst):
+    sum_lst1=0
+    sum_lst2=0
+    for i in range(len(lst)):
+        number=lst[i]%2==0
+        if number==True:
+            sum_lst1+=lst[i]
+        elif number==False:
+            sum_lst2+=lst[i]
+    difference=sum_lst1-sum_lst2
+    return difference
+result=even_odd_difference([1,2,3,4,5,6,7,8,9,10])
+print(result)
