349cff49fc7c7770abd3564cde6097e539aba2e9
Aalekseika <aleksieienkonatalia@gmail.com>
2016-12-10 15:22:47 +0200

    Update hw#23.py

--- hw#23.py
+++ hw#23.py
@@ -1,7 +1,7 @@
 def avr_num(lst):
     sum_lst = 0
     for i in range(len(lst)):
-        sum_lst+=i
+        sum_lst+=lst[i]
     average = sum_lst/len(lst)
     return average
 result=avr_num([1,2,3,4,5,6,7,8,9,10])
