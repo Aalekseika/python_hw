52d1b26988ce0236069daec004c579ebeb9cd4ac
Aalekseika <aleksieienkonatalia@gmail.com>
2016-12-10 15:19:21 +0200

    Commit#19

---
+++ hw#19.py
@@ -0,0 +1,14 @@
+#вывести на экран все простые числа от 1 до 100
+
+import math
+for i in range(2,101):
+    for k in range(2,i):
+        if k>int(math.sqrt(i)):
+            print(i)
+            break
+        elif i%2==0:
+            break
+        elif i%k==0:
+            break
+    else:
+        print(i)
