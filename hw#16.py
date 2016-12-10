522ba9abf9e7b1bc4df5ff2ff2feca80d42b182e
Aalekseika <aleksieienkonatalia@gmail.com>
2016-12-10 15:17:58 +0200

    Commit#16

---
+++ hw#16.py
@@ -0,0 +1,12 @@
+A=ord('A')
+B=ord('B')
+C=ord('C')
+D=ord('D')
+def sum_numbers(A,D):
+    accumulator = 0
+    for i in range(A,D+1):
+        if A<=i<=D:
+            accumulator+=i
+    return accumulator
+result=sum_numbers(A,D)
+print("Sum of number codes Unicodes's symbols from %s to %s is equal %d:" % ('A','D',result))
