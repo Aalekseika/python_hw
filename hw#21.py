679727d94fb6f7782d0be18fb0d293faf61fd068
Aalekseika <aleksieienkonatalia@gmail.com>
2016-12-11 22:23:09 +0200

    Update hw#21.py

--- hw#21.py
+++ hw#21.py
@@ -1,27 +1,16 @@
 #вывести все числа от 0 до 1000, которые содержат в себе 1 и 7
 
 for i in range(10,1000):
+    num = i
     _1_found = False
     _7_found = False
-    digit = i % 10
-    if digit==1:
-        _1_found=True
-    elif digit==7:
-        _7_found = True
-    digit = i // 10
-    if digit==1:
-        _1_found=True
-    elif digit==7:
-        _7_found = True
-    digit = i % 100//10
-    if digit == 1:
-        _1_found = True
-    elif digit == 7:
-        _7_found = True
-    digit = i//100
-    if digit == 1:
-        _1_found = True
-    elif digit == 7:
-        _7_found = True
+
+    while (i>0):
+        digit = i % 10
+        i = i // 10
+        if digit == 1:
+            _1_found = True
+        elif digit == 7:
+            _7_found = True
     if _1_found == True and _7_found == True:
-        print(i)
+        print(num)
