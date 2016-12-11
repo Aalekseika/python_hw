64e617c6d770477e51d9ceb8dba47dee5e61c9cf
Aalekseika <aleksieienkonatalia@gmail.com>
2016-12-11 22:08:48 +0200

    Update hw#19.py

--- hw#19.py
+++ hw#19.py
@@ -1,14 +1,16 @@
 #вывести на экран все простые числа от 1 до 100
 
 import math
-for i in range(2,101):
-    for k in range(2,i):
-        if k>int(math.sqrt(i)):
+def primes():
+    for i in range(2,101):
+        for k in range(2,i):
+            if k>int(math.sqrt(i)):
+                print(i)
+                break
+            elif i%2==0:
+                break
+            elif i%k==0:
+                break
+        else:
             print(i)
-            break
-        elif i%2==0:
-            break
-        elif i%k==0:
-            break
-    else:
-        print(i)
+primes()
